bdb8fa6b3a9f6680af4f6896d4651bcff2a2a757
Arthur <jamacedo1@stefanini.com>
2021-03-31 21:27:47 -0300

    Versão mais recente

--- src/main/java/com/sca/seguranca_comunicacao/config/RootRedirectController.java
+++ src/main/java/com/sca/seguranca_comunicacao/config/RootRedirectController.java
@@ -1,11 +1,13 @@
-package com.sca.seguranca_comunicacao;
+package com.sca.seguranca_comunicacao.config;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 @RequestMapping("/")
+@ApiIgnore
 public class RootRedirectController {
     @GetMapping
     public String swaggerUi() {
